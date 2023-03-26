f1402630c3a1d8dc0fd0456bb3f8a5aa65775f0d
Kiesaro <freestylerspb@gmail.com>
2023-03-26 19:23:58 +0300

    first commmit

---
+++ example66/Program.cs
@@ -0,0 +1,23 @@
+﻿// Example 66 
+Console.Clear();
+void Sum(int m, int n)
+{
+    Console.Write(Result(m - 1, n));
+}
+int Result(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + Result(m, n);
+        return res;
+    }
+}
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Sum(m, n);
---
+++ example68/Program.cs
@@ -0,0 +1,27 @@
+﻿//Wxmple 68
+Console.Clear();
+void Result(int m, int n)
+{
+    Console.Write(func(m, n)); 
+}
+
+int func(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return func(m - 1, 1);
+    }
+    else
+    {
+        return (func(m - 1, func(m, n - 1)));
+    }
+}
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Result(m,n);
